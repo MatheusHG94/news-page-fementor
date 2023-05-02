2c02f700c3682af5e348941842774516d32763e5
Matheus <mat.hen.gra@gmail.com>
2023-05-02 16:42:12 -0300

    recent news css

--- src/components/MainNews/index.jsx
+++ src/components/MainNews/index.jsx
@@ -4,7 +4,7 @@ import Button from 'components/Button'
 
 export default function MainNews() {
   return (
-    <section>
+    <section className={styles.mainNews} >
         <h1>The Bright Future of Web 3.0?</h1>
         <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
         <Button >Read more</Button>
