7b6df2dc5a261f241d345b39537429957f5918ef
Matheus <mat.hen.gra@gmail.com>
2023-05-20 12:34:59 -0300

    menu desktop

--- src/components/Header/Menu/index.jsx
+++ src/components/Header/Menu/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Menu.module.scss'
 import menuClose from './icon-menu-close.svg'
 import menu from './icon-menu.svg'
@@ -8,38 +8,53 @@ export default function Menu() {
   const [isActive, setActive] = useState(false);
 
   const toggleMenu = () => {
-    if(isActive) {
+    if (isActive) {
       setActive(false);
     } else {
       setActive(true);
     }
   }
 
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  
-  return (
-    <div className={styles.Menu} >
-      <button onClick={() => toggleMenu()} >
-        <img src={menu} alt="Hamburger menu icon" />
-      </button>
-      <nav className={isActive ? styles.Menu_list : styles.Menu_list___hidden} >
-        <button onClick={() => toggleMenu()} >
-          <img src={menuClose} alt="Close menu icon" />
-        </button>
-        <ul>
-          <li>Home</li>
-          <li>New</li>
-          <li>Popular</li>
-          <li>Trendind</li>
-          <li>Categories</li>
-        </ul>
-      </nav>
-      <div className={isActive ? styles.Menu_backdrop___active : styles.Menu_backdrop} ></div>
-    </div>
-  )}
+  const [screenWidth, getScreenWidth] = useState(window.innerWidth);
+  const setScreenWidth = () => getScreenWidth(window.innerWidth);
 
-/*   return (
-    <nav>
+  useEffect(() => {
+    window.addEventListener('resize', setScreenWidth);
+
+    return (
+      () => {
+        window.removeEventListener('resize', setScreenWidth);
+      }
+    )
+  }, [screenWidth])
+
+  console.log(screenWidth);
+
+  if (screenWidth < 1024) {
+    return (
+      <div className={styles.Menu} >
+        <button onClick={() => toggleMenu()} >
+          <img src={menu} alt="Hamburger menu icon" />
+        </button>
+        <nav className={isActive ? styles.Menu_list : styles.Menu_list___hidden} >
+          <button onClick={() => toggleMenu()} >
+            <img src={menuClose} alt="Close menu icon" />
+          </button>
+          <ul>
+            <li>Home</li>
+            <li>New</li>
+            <li>Popular</li>
+            <li>Trendind</li>
+            <li>Categories</li>
+          </ul>
+        </nav>
+        <div className={isActive ? styles.Menu_backdrop___active : styles.Menu_backdrop} ></div>
+      </div>
+    )
+  }
+
+  return (
+    <nav className={styles.Menu_desktop}>
       <ul>
         <li>Home</li>
         <li>New</li>
@@ -48,4 +63,6 @@ export default function Menu() {
         <li>Categories</li>
       </ul>
     </nav>
-  ) */
+  )
+
+}
